f93fb3175b3a0e0582d9927d5682a5b8f072c37e
Ankit Tyagi <ankit.tyagi.2@gmail.com>
2017-01-17 21:36:06 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# ECR Upload To S3
+
+Uploads ECR Docker credentials to S3. Inspired by [adobe-platform/aws-ecr-login](https://github.com/adobe-platform/aws-ecr-login)
+
+Effectively creates a docker.tar.gz file from the authorization that is used when accessing private ECR repositories in [Marathon](https://mesosphere.github.io/marathon/) or [DC/OS](https://dcos.io).
+
+## Environment Variables
+
+Use these to change the behavior of the container.
+
+### Required
+- `REGISTRIES` - The 10 digit account id for the registry
+- `S3_BUCKET` - The S3 bucket where the the file will live
+
+### Optional
+- `INTERVAL` - Change the interval, in seconds, for when the credentials will get updated. Defaults to 21600 (6 hours)
+- `S3_PATH` - An optional path that sits underneath the S3 bucket 
+- `AWS_ACCESS_KEY_ID` - 
+- `AWS_SECRET_ACCESS_KEY` - 
+
+## Examples for Running
+
+```
+docker run \
+    -e "REGISTRIES=1234567890" \
+    -e "S3_BUCKET=my-awesome-bucket" \
+    atyagi/ecr-upload-to-s3
+```
+
+## Marathon JSON Example
+
+```json
+{
+    "id": "ecr-upload-to-s3",
+    "cpus": 0.1,
+    "mem": 64,
+    "instances": 1,
+    "container": {
+        "docker": {
+            "network": "HOST",
+            "image": "atyagi/ecr-upload-to-s3:latest"
+        }
+    },
+    "env": {
+        "REGISTRIES": "1234567890",
+        "S3_BUCKET": "my-awesome-bucket"
+    }
+}
+```
---
+++ ecr-login.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+
+echo "Starting now"
+
+function usage {
+    echo "usage: $0 [options]"
+    echo "       -r|--region        If not provided, the region will be looked up via AWS metadata (optional on EC2-only)."
+    echo "       -g|--registries    The AWS account IDs to use for the login. Space separated. (Ex: \"123456789101, 98765432101\")"
+    echo "       -i|--interval      How often to loop and refresh credentials (optional - default is 21600 - 6 hours)."
+    exit 1
+}
+
+function log {
+    echo $(date -u) $1
+}
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case ${key} in
+    -r|--region)
+    REGION="$2"
+    shift;;
+    -g|--registries)
+    REGISTRIES="$2"
+    shift;;
+    -i|--interval)
+    INTERVAL="$2"
+    shift;;
+    *)
+    # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+if [ -z "$REGISTRIES" ]; then
+    echo "Registry IDs are required."
+    usage
+fi
+
+if [ -z "$S3_BUCKET" ]; then
+    echo "S3 Bucket is required."
+    usage
+fi
+
+if [ -z "$REGION" ]; then
+    AZ=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+    REGION=${AZ%?}
+
+    if [[ -z "$REGION" ]]; then
+        echo "Region could not be determined."
+        usage
+    fi
+    
+    AWS_DEFAULT_REGION=${REGION}
+fi
+
+if [[ -z "$INTERVAL" ]]; then
+    log "Custom interval not provided, defaulting to 21600 seconds - 6 hours"
+    INTERVAL=21600
+fi
+
+JSON_LOCATION="/docker/.docker/config.json"
+
+while true; do
+    for ACCOUNT_NUM in ${REGISTRIES}
+    do
+        log "Login started for account: $ACCOUNT_NUM"
+        ECR_AUTH=$(aws ecr get-authorization-token --region ${REGION} --registry-ids ${ACCOUNT_NUM} --output json)
+
+        AUTH_TOKEN=$(echo ${ECR_AUTH} | jq -r '.authorizationData[0].authorizationToken')
+        ENDPOINT=$(echo ${ECR_AUTH} | jq -r '.authorizationData[0].proxyEndpoint')
+
+        log "Endpoint found: $ENDPOINT"
+
+        if [[ -z "$AUTH_TOKEN" || -z "$ENDPOINT" ]]; then
+            log "Unable to locate ECR login auth information for account: $ACCOUNT_NUM. Skipping..."
+            continue;
+        fi
+
+        ECR_JSON="{\"auths\": {\"$ENDPOINT\": {\"auth\": \"$AUTH_TOKEN\",\"email\": \"none\"}}}"
+        ECR_JSON_PLAIN="{\"auth\": \"$AUTH_TOKEN\",\"email\": \"none\"}"
+
+        # If a dockercfg file doesn't already exist (odd), we can just write and exit
+        if [[ ! -f ${JSON_LOCATION} || ! -s ${JSON_LOCATION} ]]; then
+            log "Docker config does not exist at $JSON_LOCATION, creating file"
+
+            mkdir -p "$(dirname "$JSON_LOCATION")"
+            echo "$ECR_JSON" > ${JSON_LOCATION}
+            continue;
+        fi
+
+        log "Existing Docker config found at $JSON_LOCATION, updating file"
+
+        # Otherwise, need to append or modify the new config to existing
+        EXISTING_CFG=$(cat ${JSON_LOCATION})
+        NEW_CONFIG=$(echo ${EXISTING_CFG} | jq ".auths[\"$ENDPOINT\"]=$ECR_JSON_PLAIN")
+
+        echo ${NEW_CONFIG} > ${JSON_LOCATION}
+
+        log "Done credential update for account: $ACCOUNT_NUM"
+    done
+
+    # Create the final .tar.gz
+    log "Creating Docker tar at /docker/docker.tar.gz"
+    tar czf /docker/docker.tar.gz /docker/.docker
+
+    if [ -z "$S3_PATH" ]; then
+        log "Uploading to s3://$S3_BUCKET/docker.tar.gz"
+        aws s3 cp /docker/docker.tar.gz s3://${S3_BUCKET}/docker.tar.gz
+    else
+        log "Uploading to s3://$S3_BUCKET/$S3_PATH/docker.tar.gz"
+        aws s3 cp /docker/docker.tar.gz s3://${S3_BUCKET}${S3_PATH}/docker.tar.gz
+    fi
+
+    log "Sleeping for $INTERVAL"
+    sleep ${INTERVAL}
+
+    log "Removing /docker/docker.tar.gz"
+    rm -f /docker/docker.tar.gz
+done
